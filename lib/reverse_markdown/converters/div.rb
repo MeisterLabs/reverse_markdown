c9f895725c98d3959a9593695ad8ff3cff4f8586
Neel Shah <neelshah.sa@gmail.com>
2019-07-05 16:22:39 +0200

    Single br div newline

--- lib/reverse_markdown/converters/div.rb
+++ lib/reverse_markdown/converters/div.rb
@@ -2,11 +2,19 @@ module ReverseMarkdown
   module Converters
     class Div < Base
       def convert(node, state = {})
+        # Quill makes an extra br inside an empty div..
+        return "\n" if single_br_child?(node)
         "\n" << treat_children(node, state)
       end
     end
 
     register :div,     Div.new
     register :article, Div.new
+
+    private
+
+    def single_br_child?(node)
+      node.children.size == 1 && node.children.first.name == 'br'
+    end
   end
 end
